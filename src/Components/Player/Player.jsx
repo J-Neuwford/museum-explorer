39c3746bae5a1d9cf8a746568fc5192d99021891
Josh Neuwford <116881112+J-Gosling@users.noreply.github.com>
2023-06-07 17:07:34 +0100

    added player movement

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,8 +1,74 @@
+import React, { useState, useEffect } from 'react';
 import './Player.css';
 
-const Player = () => {
-return (
-  <div className="player"> PLAYER </div>
-)
-}
+const Player = (props) => {
+  const [playerPosition, setPlayerPosition] = useState({x: 0, y: 0})
+  const [pressedKeys, setPressedKeys] = useState([]);
+  const moveSpeed = 5;
+
+  const handleKeyDown = (event) => {
+    const key = event.key.toLowerCase();
+    if (!pressedKeys.includes(key)) {
+      setPressedKeys((prevKeys) => [...prevKeys, key]);
+    }
+  };
+
+  const handleKeyUp = (event) => {
+    const key = event.key.toLowerCase();
+    setPressedKeys((prevKeys) => prevKeys.filter((k) => k !== key));
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, []);
+
+  useEffect(() => {
+    let animationFrameId = null;
+    let x = playerPosition.x;
+    let y = playerPosition.y;
+
+    const movePlayer = () => {
+      if (pressedKeys.includes('w')) {
+        y -= moveSpeed;
+      }
+      if (pressedKeys.includes('a')) {
+        x -= moveSpeed;
+      }
+      if (pressedKeys.includes('s')) {
+        y += moveSpeed;
+      }
+      if (pressedKeys.includes('d')) {
+        x += moveSpeed;
+      }
+      setPlayerPosition({ x: x, y: y });
+      animationFrameId = requestAnimationFrame(movePlayer);
+    };
+
+    if (pressedKeys.length > 0) {
+      animationFrameId = requestAnimationFrame(movePlayer);
+    }
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, [pressedKeys]);
+
+  return (
+    <div
+      className="player"
+      style={{
+        transform: `translate(${playerPosition.x}px, ${playerPosition.y}px)`,
+      }}
+    >
+      Player
+    </div>
+  );
+};
+
 export default Player;
