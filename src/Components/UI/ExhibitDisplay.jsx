212f42220b16848d719d62257a6bfb464d7d8645
Josh Neuwford <116881112+J-Gosling@users.noreply.github.com>
2023-06-14 18:37:53 +0100

    added API data from a single exhibit to the exhibit displays

--- src/Components/UI/ExhibitDisplay.jsx
+++ src/Components/UI/ExhibitDisplay.jsx
@@ -1,9 +1,38 @@
+import React, { useEffect, useState } from 'react'
 import './ExhibitDisplay.css'
+import axios from 'axios'
 
-const ExhibitDisplay = () => {
+const ExhibitDisplay = ({ exhibitId }) => {
+  const [exhibitTitle, setExhibitTitle] = useState(null)
+  const [exhibitImage, setExhibitImage] = useState(null)
+
+  useEffect(() => {
+    const getExhibit = async () => {
+      const url = 'https://api.vam.ac.uk/v2/museumobject/O828146'
+      const imageUrl = "https://framemark.vam.ac.uk/collections/"
+      const imageSize = 120;
+
+      try {
+        const response = await axios.get(url);
+        const exhibitData = await response.data.record;
+
+        setExhibitTitle(exhibitData.titles[0].title)
+        setExhibitImage(`${imageUrl}${exhibitData.images[0]}/full/!${imageSize},${imageSize}/0/default.jpg`)
+
+        console.log(exhibitData.summaryDescription[0])
+      } catch (error) {
+        console.error('error fetching exhibit', error);
+      }
+    }
+
+    getExhibit();
+
+  }, [exhibitId])
+
   return (
     <div className="exhibit-display">
-     EXHIBIT DISPLAY
+     <div>{exhibitTitle}</div>
+     <img src={exhibitImage}/>
     </div>
   )
 }
